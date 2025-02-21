93ab962237b0e462aa07f6f31da1c41248a4540c
Michael Riley <michaelriley87@gmail.com>
2025-02-21 12:08:02 +1100

    prepare for github pages deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,18 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/picky-colour-picker' : '',
+    },
+    appDir: 'app',
+    prerender: {
+      entries: ['*']
+    }
   }
 };
 
