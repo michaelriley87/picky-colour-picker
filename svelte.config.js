bbc7db9e62b8c4f544d192e7ffa3a27ae05381fb
Michael Riley <michaelriley87@gmail.com>
2026-09-05 21:38:30 +1000

    Configure Picky custom domain

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
       fallback: 'index.html',
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/picky-colour-picker' : '',
+      base: '',
     },
     appDir: 'app',
     prerender: {
